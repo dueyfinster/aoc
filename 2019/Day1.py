e4489d71d654a1137254497429a52b8f310df629
Neil Grogan <neil@grogan.ie>
2019-12-01 20:21:44 +0000

    Solved 2019 Day 1!

---
+++ 2019/Day1.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+import os
+import math
+
+def read_file(filename):
+    with open(filename) as f:
+        content = f.readlines()
+    content = [int(x.strip()) for x in content]
+    return content
+
+
+def calculate(result, num):
+    return result + math.floor((int(num)/3)) -2
+
+def calculate_rescursive(result, payload):
+    new_payload = math.floor((int(payload)/3)) -2
+    if new_payload >= 0:
+        result = result + new_payload
+        return calculate_rescursive(result, new_payload)
+    else:
+        return result
+
+
+def part1(content):
+    result = 0
+    for num in content:
+        result = calculate(result, num)
+
+    print(result)
+
+
+
+def part2(content):
+    total_result = 0
+    for num in content:
+        total_result = total_result + calculate_rescursive(0, num)
+    print(total_result)
+
+
+def main():
+    filename = os.path.abspath("2019/input/day1.txt")
+    content = read_file(filename)
+    part1(content)
+    part2(content)
+
+
+if __name__ == '__main__':
+    main()
