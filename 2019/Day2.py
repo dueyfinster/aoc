c67dc52b8e3e5cf35fbc975dbaf7572c4c3bb8da
Neil Grogan <neil@grogan.org>
2019-12-02 13:19:08 +0000

    Day 2 Part 1 Complete

--- 2019/Day2.py
+++ 2019/Day2.py
@@ -3,8 +3,34 @@ import os
 from ngu import read_file
 
 
+def part1(content):
+    for i in range(0, len(content), 4):
+        opcode = int(content[i])
+        pos1 = int(content[i+1])
+        pos2 = int(content[i+2])
+        result_pos = int(content[i+3])
+        val1 = int(content[pos1])
+        val2 = int(content[pos2])
+
+        if opcode == 1:
+            content[result_pos] = val1 + val2
+        elif opcode == 2:
+            content[result_pos] = val1 * val2
+        elif opcode == 99:
+            return content
+
+
+def part2(content):
+    pass
+
+
 def main():
-    content = read_file(2)
+    content = read_file(2, True)
+    # Replace incorrect valeus from input
+    content[1] = 12
+    content[2] = 2
+    p1_result = part1(content)[0]
+    print("Part 1: {}".format(p1_result))
 
 
 if __name__ == '__main__':
--- 2019/ngu.py
+++ 2019/ngu.py
@@ -1,10 +1,15 @@
-import os 
+import os
+import csv
 
-def read_file(day_num):
+
+def read_file(day_num, is_csv=False):
     dirname = os.path.dirname(__file__)
     filename = "input/day{}.txt".format(day_num)
     full_path = os.path.join(dirname, filename)
     with open(full_path) as f:
-        content = f.readlines()
-    content = [x.strip() for x in content]
+        if is_csv:
+            content = list(csv.reader(f, delimiter=','))[0]
+        else:
+            content = f.readlines()
+            content = [x.strip() for x in content]
     return content
