773b49fd900d3165f846f24f98c784af048ba9bb
Neil Grogan <neil@grogan.org>
2019-12-02 13:49:09 +0000

    Day 2 - Part 2 complete

--- 2019/Day2.py
+++ 2019/Day2.py
@@ -20,17 +20,32 @@ def part1(content):
             return content
 
 
-def part2(content):
-    pass
+def part2(orig_content):
+    answer_sought = 19690720
+    for i in range(12, 99):
+        for x in range(2, 99):
+            content = orig_content.copy()
+            content[1] = i
+            content[2] = x
+            result = part1(content)[0]
+
+            if result == answer_sought:
+                print("Part 2 verb: {} noun: {}".format(i, x))
+                return 100 * i + x
 
 
 def main():
-    content = read_file(2, True)
+    p1_content = read_file(2, True)
+    p2_content = p1_content.copy()
+
+    p1_content[1] = 12
+    p1_content[2] = 2
+
     # Replace incorrect valeus from input
-    content[1] = 12
-    content[2] = 2
-    p1_result = part1(content)[0]
+    p1_result = part1(p1_content)[0]
     print("Part 1: {}".format(p1_result))
+    p2_result = part2(p2_content)
+    print("Part 2: {}".format(p2_result))
 
 
 if __name__ == '__main__':
