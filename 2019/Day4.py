8402e7254ece3759608f077e381916ab4048d321
Neil Grogan <neil@grogan.org>
2019-12-04 12:55:36 +0000

    Day 4 Part 1 Done

--- 2019/Day4.py
+++ 2019/Day4.py
@@ -1,21 +1,46 @@
 #!/usr/bin/env python3
 from ngu import read_file
+from collections import Counter
 
 
-def part1():
-    pass
+def all_digits_increasing(nums):
+    if nums != sorted(nums):
+        return False
+
+    return True
 
 
-def part2():
+def repeated_nums_adjacent(nums):
+    last_num = nums[0]
+    for num in nums[1:]:
+        if last_num == num:
+            return True
+        last_num = num
+    return False
+
+
+def part1(start, end):
+    candidates = []
+    start_nums = [int(i) for i in str(start)]
+    for number in range(start, end+1):
+        nums = [int(i) for i in str(number)]
+        if all_digits_increasing(nums):
+            if repeated_nums_adjacent(nums):
+                candidates.append(number)
+
+    return len(candidates)
+
+
+def part2(start, end):
     pass
 
 
 def main():
     content = read_file(4)[0].split('-')
-
-    p1_result = part1(content)
+    start, end = int(content[0]), int(content[1])
+    p1_result = part1(start, end)
     print("Part 1: {}".format(p1_result))
-    p2_result = part2(content)
+    p2_result = part2(start, end)
     print("Part 2: {}".format(p2_result))
 
 
