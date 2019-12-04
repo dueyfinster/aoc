3be813fd1450de93afe3194f137cc0a5b6081855
Neil Grogan <neil@grogan.org>
2019-12-04 11:09:13 +0000

    Completed Day3!

--- 2019/Day3.py
+++ 2019/Day3.py
@@ -7,7 +7,7 @@ def manhattan_dist(pos):
     return abs(pos[0]) + abs(pos[1])
 
 
-def convert_to_coords(positions, last_pos, pos_to_calculate, distance):
+def convert_to_coords(positions, last_pos, pos_to_calculate, distance, crossings, distances):
     movement = int(re.sub("R|L|U|D", "", pos_to_calculate))
     x, y = last_pos
     for _ in range(0, abs(movement)):
@@ -26,44 +26,40 @@ def convert_to_coords(positions, last_pos, pos_to_calculate, distance):
         new_pos = (x, y)
         positions.add(new_pos)
         distance = distance + 1
+        if new_pos in crossings:
+            distances[new_pos] = distance
 
     return new_pos, distance
 
 
-def calculate_positions(wire, distances):
+def calculate_positions(wire, crossings):
     positions = set()
     start_pos = (0, 0)
+    distances = dict()
     distance = 0
     for pos_to_calc in wire:
         curr_pos, distance = convert_to_coords(
-            positions, start_pos, pos_to_calc, distance)
+            positions, start_pos, pos_to_calc, distance, crossings, distances)
         start_pos = curr_pos
-        distances[curr_pos] = distance
 
-    return positions
+    return positions, distances
 
 
 def part1(wire1, wire2):
-    w1_positions = calculate_positions(wire1, {})
-    w2_positions = calculate_positions(wire2, {})
+    w1_positions, _ = calculate_positions(wire1, {})
+    w2_positions, _ = calculate_positions(wire2, {})
     crossings = w1_positions.intersection(w2_positions)
     man_distances = [manhattan_dist(pos) for pos in crossings]
-    return min(man_distances)
+    return min(man_distances), crossings
 
 
-def part2(wire1, wire2):
+def part2(wire1, wire2, crossings):
     lowest_cost_crossing = 0
-    w1_distances = {}
-    w1_positions = calculate_positions(wire1, w1_distances)
-    w2_distances = {}
-    w2_positions = calculate_positions(wire2, w2_distances)
-    crossings = w1_positions.intersection(w2_positions)
+    w1_positions, w1_distances = calculate_positions(wire1, crossings)
+    w2_positions, w2_distances = calculate_positions(wire2, crossings)
 
-    crossed_dist = {}
-
-    for crossing in crossings:
-        # TODO
-        print(w1_distances[crossing])
+    distances = [w1_distances[pos] + w2_distances[pos] for pos in crossings]
+    lowest_cost_crossing = min(distances)
 
     return lowest_cost_crossing
 
@@ -73,9 +69,9 @@ def main():
     wire1 = content[0].split(",")
     wire2 = content[1].split(",")
 
-    p1_result = part1(wire1, wire2)
+    p1_result, crossings = part1(wire1, wire2)
     print("Part 1: {}".format(p1_result))
-    p2_result = part2(wire1, wire2)
+    p2_result = part2(wire1, wire2, crossings)
     print("Part 2: {}".format(p2_result))
 
 
