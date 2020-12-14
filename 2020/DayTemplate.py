c7bd6df800b90433905f707e043128eb35de6a59
Neil Grogan <neil@grogan.org>
2020-12-14 16:54:45 +0000

    Added Day Template

---
+++ 2020/DayTemplate.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+def process_input(content):
+    pass
+
+
+def part1(data):
+    pass
+    #print("Part 1: {}".format(str(result)))
+
+
+def part2(data):
+    pass
+    #print("Part 2: {}".format(str(result)))
+
+
+def main():
+    #content = read_file(3)
+    data = process_input(content)
+    part1(data)
+    part2(data)
+
+
+if __name__ == '__main__':
+    main()
