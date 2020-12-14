fd2f492f8a859904f2aeabffc5bfd8df01d12cff
Neil Grogan <neil@grogan.org>
2020-12-14 16:48:40 +0000

    Completed Day3 2020

--- 2020/Day3.py
+++ 2020/Day3.py
@@ -12,22 +12,34 @@ def process_input(content):
     return data
 
 
-def part1(data):
+def nav_slopes(right, down, data):
     x = 0
     result = 0
     width = len(data[0])
 
-    for row in data:
+    for i, row in enumerate(data):
+        if i % down != 0:
+            continue
         if row[x] == '#':
             result = result + 1
-        x = (x + 3) % width
+        x = (x + right) % width
+    return result
 
+
+def part1(data):
+    result = nav_slopes(3, 1, data)
     print("Part 1: {}".format(str(result)))
 
 
 def part2(data):
-    pass
-    #print("Part 2: {}".format(str(result)))
+    r1 = nav_slopes(1, 1, data)
+    r2 = nav_slopes(3, 1, data)
+    r3 = nav_slopes(5, 1, data)
+    r4 = nav_slopes(7, 1, data)
+    r5 = nav_slopes(1, 2, data)
+
+    result = r1 * r2 * r3 * r4 * r5
+    print("Part 2: {}".format(str(result)))
 
 
 def main():
