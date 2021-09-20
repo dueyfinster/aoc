343386371a9c6c7ed4d4a28158032a9fdcec7951
Neil Grogan <neil.grogan@ericsson.com>
2021-09-20 14:38:15 +0100

    First commit of Java Project

---
+++ src/main/java/2015/Day1.java
@@ -0,0 +1,42 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+class Day1 {
+
+    public static void main(String[] args) throws IOException {
+        Day1 d1 = new Day1();
+        String input = readInFile();
+        System.out.println("Day 1, Part 1: " + d1.Part1(input));
+        System.out.println("Day 1, Part 2: " + d1.Part2(input));
+    }
+    public int Part1(String input) {
+        long openBrackets = input.chars().filter(x -> x == '(').count();
+        long closedBrackets = input.chars().filter(x -> x == ')').count();
+        return (int) (openBrackets - closedBrackets);
+    }
+
+    public int Part2(String input) {
+        char[] c = input.toCharArray();
+        int runningTotal = 0;
+        for(int i=0; i<c.length; i++){
+            if(c[i] =='('){
+                runningTotal += 1;
+            }else if(c[i] ==')'){
+                runningTotal -= 1;
+            }
+
+            if(runningTotal == -1){
+                return i+1;
+            }
+        }
+        return 0;
+    }
+
+    private static String readInFile() throws IOException {
+        Path path = Paths.get("src/main/resources/2015/Day1.txt");
+        String input = Files.readAllLines(path).get(0);
+        return input;
+    }
+}
---
+++ src/test/java/2015/Day1Tests.java
@@ -0,0 +1,48 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+class Day1Tests {
+
+    @Test
+    @DisplayName("(()) = 0")
+    void part1bracketsCancelOut() {
+        Day1 d1 = new Day1();
+        assertEquals(0, d1.Part1("(())"));
+    }
+
+    @Test
+    @DisplayName("Input File = 280")
+    void part1ActualResult() throws IOException {
+        Day1 d1 = new Day1();
+        Path path = Paths.get("src/main/resources/2015/Day1.txt");
+        String input = Files.readAllLines(path).get(0);
+        assertEquals(280, d1.Part1(input));
+    }
+
+
+    @Test
+    @DisplayName("(())) = 5")
+    void part2LastIndexBasementFloor() {
+        Day1 d1 = new Day1();
+        assertEquals(5, d1.Part2("(()))"));
+    }
+
+    @Test
+    @DisplayName("Input File = 1797")
+    void part2ActualResult() throws IOException {
+        Day1 d1 = new Day1();
+        Path path = Paths.get("src/main/resources/2015/Day1.txt");
+        String input = Files.readAllLines(path).get(0);
+        assertEquals(1797, d1.Part2(input));
+    }
+
+}
