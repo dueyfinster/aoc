4a93d9a4181acbd58e4a4c127f0ad2216481b5bb
Neil Grogan <neil@grogan.org>
2021-12-20 21:13:35 +0000

    Start of Day 1 2021

---
+++ 2021/Day1.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+import os
+import math
+from ngu import read_file
+from itertools import combinations
+
+
+def part1(content):
+   # combos = list(combinations(content, 2))
+   # x, y = list(filter(lambda val: sum(val) == 2020, combos))[0]
+   # result = x * y
+
+    print("Part 1: {}".format(str(result)))
+
+
+def part2(content):
+   # combos = list(combinations(content, 3))
+   # x, y, z = list(filter(lambda val: sum(val) == 2020, combos))[0]
+   # result = x * y * z
+
+    print("Part 2: {}".format(str(result)))
+
+
+def main():
+    content = read_file(1)
+    nums = list(map(int, content))
+    part1(nums)
+    part2(nums)
+
+
+if __name__ == '__main__':
+    main()
