f447338c42d0c2fd8a6b7911cff07393844d256c
Neil Grogan <neil@grogan.org>
2022-05-26 14:22:34 +0100

    Update import from test

--- aoc17/test_2017.py
+++ aoc17/test_2017.py
@@ -2,7 +2,7 @@ import unittest
 import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-import Day1, Day2
+from . import Day1, Day2
 
 class Day1Tests(unittest.TestCase):
 
