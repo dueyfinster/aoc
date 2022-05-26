e2ad0991ace4d07734a35af883b49fe9a5f21957
Neil Grogan <neil@grogan.org>
2022-05-26 14:48:17 +0100

    Added more year placeholder tests

---
+++ aoc21/test_2021.py
@@ -0,0 +1,22 @@
+import unittest
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+#from . import Day1, Day2
+
+class Day1Tests(unittest.TestCase):
+
+    @unittest.skip("Not Implemented")
+    def test_day_one_part_one(self):
+        pass
+        #self.assertEqual(Day1.part_one(), 1223)
+    
+    @unittest.skip("Not Implemented")
+    def test_day_two_part_one(self):
+        pass
+        #self.assertEqual(Day1.part_two(), 1284)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
