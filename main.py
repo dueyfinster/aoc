b004a8ac4649e6cfbbaaa26157fbb70bd18411e1
Neil Grogan <neil@grogan.org>
2022-11-15 12:16:26 +0000

    Fixed main to run files

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 #!/usr/bin/env python3
 import os
+import glob
 from subprocess import call
 
 def list_files():
-    return [each for each in os.listdir("2019") if each.endswith('.py')]
+    listing = glob.glob('[2][0][1-2][0-9]\/*')
+    
+    return [each for each in listing if each.endswith('.py') and not 'init' in each and not 'test' in each]
 
 def run_script(script):
     call(["python", script])
 
 def main():
-	files = list_files()
-	for file in files:
-	    run_script("2019/"+file)
+    files = list_files()
+    print(files)
+    for file in files:
+        run_script(file)
 
 
 if __name__ == '__main__':
