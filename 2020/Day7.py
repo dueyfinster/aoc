5346711f18a37e0a41343d17da2f8db5100c77e2
Neil Grogan <neil@grogan.org>
2022-11-19 13:29:57 +0000

    Started Day7 Pt 1 2020

--- 2020/Day7.py
+++ 2020/Day7.py
@@ -3,10 +3,24 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from utils import advent, helper
-
+import re
+from itertools import cycle
 
 def part1(data):
     result = None
+    regex = re.compile('(?!bag|bags|contain)(?<![a-zA-Z])[0-9a-zA-Z]+')
+
+    li = list(map(lambda s: regex.findall(s), data))
+
+    main_keys = ['adj', 'col']
+    sub_keys = ['no', 'adj', 'col']
+    nli = []
+
+    for values in iter(li):
+       d = dict(zip(main_keys, values[0:2]))
+       d['con'] = tuple(zip(cycle(sub_keys), values[2:]))
+       nli.append(d)
+
     advent.print_answer(1, result)
     return result
 
